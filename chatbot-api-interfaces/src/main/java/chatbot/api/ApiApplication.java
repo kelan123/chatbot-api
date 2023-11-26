0dbc7913779513148a3321fa6401414ea777e54d
kelan123 <1145980140@qq.com>
2023-11-26 23:24:52 +0800

    feat:init ddd project

---
+++ chatbot-api-interfaces/src/main/java/chatbot/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author : 柯兰
+ * @Time : 2023/11/26 22:04
+ * @Description :启动入口
+ **/
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/chatbot/api/test/ApiTest.java
@@ -0,0 +1,9 @@
+package chatbot.api.test;
+
+/**
+ * @Author : 柯兰
+ * @Time : 2023/11/26 22:20
+ * @Description :单元测试
+ **/
+public class ApiTest {
+}
